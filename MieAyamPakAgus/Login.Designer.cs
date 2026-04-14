9de53ab2e3fdde0b251a61cb0d626191a44512d9
yuumuu <haidar.alhazen93@gmail.com>
2026-04-15 06:48:12 +0700

    Create a Login Page UI

---
+++ MieAyamPakAgus/Login.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace MieAyamPakAgus
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblLogin = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputUsername = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblLogin
+            // 
+            this.LblLogin.AutoSize = true;
+            this.LblLogin.Font = new System.Drawing.Font("Bricolage Grotesque 14pt", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLogin.Location = new System.Drawing.Point(676, 174);
+            this.LblLogin.Name = "LblLogin";
+            this.LblLogin.Size = new System.Drawing.Size(300, 58);
+            this.LblLogin.TabIndex = 0;
+            this.LblLogin.Text = "Login as Admin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bricolage Grotesque 14pt", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(506, 306);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(177, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bricolage Grotesque 14pt", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(506, 459);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 50);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // InputUsername
+            // 
+            this.InputUsername.Font = new System.Drawing.Font("Bricolage Grotesque 14pt", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputUsername.Location = new System.Drawing.Point(515, 366);
+            this.InputUsername.Margin = new System.Windows.Forms.Padding(10);
+            this.InputUsername.Name = "InputUsername";
+            this.InputUsername.Size = new System.Drawing.Size(639, 46);
+            this.InputUsername.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Bricolage Grotesque 14pt", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(515, 519);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(639, 46);
+            this.textBox1.TabIndex = 4;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Font = new System.Drawing.Font("Bricolage Grotesque 14pt SemiBo", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLogin.Location = new System.Drawing.Point(708, 664);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(250, 100);
+            this.BtnLogin.TabIndex = 5;
+            this.BtnLogin.Text = "Login";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1678, 1067);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.InputUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LblLogin);
+            this.MinimumSize = new System.Drawing.Size(0, 10);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblLogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InputUsername;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnLogin;
+    }
+}
+
