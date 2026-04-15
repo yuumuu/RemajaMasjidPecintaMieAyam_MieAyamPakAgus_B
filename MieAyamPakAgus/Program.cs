1e0b46e0e960ff5cf70f3685d62183602d209d93
yuumuu <haidar.alhazen93@gmail.com>
2026-04-15 13:10:36 +0700

    Hapus library ga kepake

--- MieAyamPakAgus/Program.cs
+++ MieAyamPakAgus/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 
 namespace MieAyamPakAgus
 {
+    /// <summary>
+    /// Aplikasi Manajemen Reservasi Mie Ayam Pak Agus
+    /// Versi: 1.0.0
+    /// </summary>
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// Titik masuk utama aplikasi.
         /// </summary>
         [STAThread]
         static void Main()
