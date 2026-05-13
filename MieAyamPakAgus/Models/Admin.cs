b298369bd4c4639a39c7adfed75dc1ae25f809df
yuumuu <haidar.alhazen93@gmail.com>
2026-05-13 12:48:45 +0700

    feat: add Admin model

---
+++ MieAyamPakAgus/Models/Admin.cs
@@ -0,0 +1,9 @@
+namespace MieAyamPakAgus.Models
+{
+    public class Admin
+    {
+        public int id_user { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
