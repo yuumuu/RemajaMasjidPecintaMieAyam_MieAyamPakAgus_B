4a82db572b049636441ea415089d25cc8ed2b870
yuumuu <haidar.alhazen93@gmail.com>
2026-05-13 12:48:44 +0700

    feat: add Session class for user state and superadmin flag

---
+++ MieAyamPakAgus/Session.cs
@@ -0,0 +1,9 @@
+namespace MieAyamPakAgus
+{
+    public static class Session
+    {
+        public static int IdUser { get; set; }
+        public static string Username { get; set; }
+        public static bool IsSuperadmin { get; set; }
+    }
+}
