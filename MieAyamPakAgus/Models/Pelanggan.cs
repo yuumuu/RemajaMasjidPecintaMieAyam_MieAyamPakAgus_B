549344efe3bb663508022b4e81f3e26e81d50fe6
yuumuu <haidar.alhazen93@gmail.com>
2026-05-13 12:48:45 +0700

    feat: add Pelanggan model

---
+++ MieAyamPakAgus/Models/Pelanggan.cs
@@ -0,0 +1,9 @@
+namespace MieAyamPakAgus.Models
+{
+    public class Pelanggan
+    {
+        public int id_pelanggan { get; set; }
+        public string nama { get; set; }
+        public string no_telepon { get; set; }
+    }
+}
