8689b6531af74d7c2b4294b4eb7af68df5aa74c6
yuumuu <haidar.alhazen93@gmail.com>
2026-05-13 12:48:46 +0700

    feat: add Meja model

---
+++ MieAyamPakAgus/Models/Meja.cs
@@ -0,0 +1,10 @@
+namespace MieAyamPakAgus.Models
+{
+    public class Meja
+    {
+        public int id_meja { get; set; }
+        public string kode { get; set; }
+        public int kapasitas { get; set; }
+        public string status_meja { get; set; }
+    }
+}
